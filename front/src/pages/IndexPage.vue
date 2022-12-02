0c3c2560d50573904a3c074825aa3832da0eb4dc
APSDIGIT\hraoult <hugo.raoult@apside-groupe.com>
2022-12-02 09:12:16 +0100

    chgmt

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,17 +1,47 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <header class="header">
+    <h4 class="title">Dashboard</h4>
+    <div class="logo">
+      <img src="../assets/Vector.png" />
+    </div>
+  </header>
+  <body>
+    <h4 class="home-title">Bonjour, Tom Dupont ! 👋</h4>
+    <container class="card">
+      <q-card class="my-card">
+        <q-card-section class="bg-grey text-white">
+          <div class="text-h6">Courses</div>
+        </q-card-section>
+        <br />
+        <div class="text-subtitle2">
+          Cette liste ne contient aucune tâches ajoutez en une depuis la liste
+        </div>
+        <q-card-actions align="around">
+          <q-btn flat>Voir la liste complète</q-btn>
+        </q-card-actions>
+      </q-card>
+    </container>
+    <container class="card">
+      <q-card class="my-card">
+        <q-card-section class="bg-grey text-white">
+          <div class="text-h6">Courses</div>
+        </q-card-section>
+        <br />
+        <div class="text-subtitle2">
+          Cette liste ne contient aucune tâches ajoutez en une depuis la liste
+        </div>
+        <q-card-actions align="around">
+          <q-btn flat>Voir la liste complète</q-btn>
+        </q-card-actions>
+      </q-card>
+    </container>
+  </body>
+  <footer class="footer">
+    <img src="../assets/menu-left.png" />
+    <div class="menu-center">
+      <img id="pic1" class="navbar" src="../assets/menu-center.png" />
+      <img id="pic2" class="navbar" src="../assets/menu-center-plus.png" />
+    </div>
+    <img src="../assets/menu-right.png" />
+  </footer>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script>
