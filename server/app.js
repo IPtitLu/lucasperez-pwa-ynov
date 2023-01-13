e4ba84b19191592d1ae699969f445e16dc12a801
hugoty <hugo.raoult@hotmail.fr>
2023-01-13 17:44:54 +0100

    back v0

--- server/app.js
+++ server/app.js
@@ -1,22 +1,34 @@
-import '#config/database.js'
-import Koa from 'koa'
-import bodyParser from 'koa-bodyparser'
-import respond from 'koa-respond'
-import cors from '@koa/cors'
-import { API_V1_ROUTER } from '#routes/index.js'
-import serve from 'koa-static'
+import express from 'express';
+const app = express();
+import mongoose from 'mongoose';
+import Articles from './src/controllers/Articles.js'
+import Categories from './src/controllers/Categories.js';
+import Users from './src/controllers/Users.js';
+import {Login} from './src/controllers/Login.js';
+import bodyParser from "body-parser";
+import auth from './src/auth/auth.js'
+import esm from "esm"
 
-const app = new Koa()
+app.use(bodyParser.json());
 
-app
-  .use(cors('*'))
-  .use(serve('./public'))
-  .use(bodyParser())
-  .use(respond())
-  .use(API_V1_ROUTER.routes())
-  .use(API_V1_ROUTER.allowedMethods())
+mongoose
+  .connect("mongodb+srv://devuser:YNOV1234@cluster0.mlauqjx.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.get("/articles", auth , Articles.getAll);
+app.get("/articles/:id", auth, Articles.getOne);
+app.get("/users", Users.getAll);
+app.get("/users/:id", Users.getOne);
+app.get("/categories", Categories.getAll);
+app.get("/categories/:id", Categories.getOne);
+app.post('/api/login', Login.login);
 
 
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
